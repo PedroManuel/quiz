f73c6b8ef718d3b8c383d1b7ddb737e0533c6354
Pedro Manuel <pedrom.perezm@gmail.com>
2015-08-02 13:05:49 +0100

    Autoload quitando findAll

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,27 +17,23 @@ export.load=function(req, res, next, quizId){
 //GET /quizes
 exports.index=function(req,res){
   models.Quiz.findAll().then(function(quizes){
-    res.render('quizes/index.ejs', {quizes: quizes});
+    res.render('quizes/index', {quizes: quizes});
   })
 };
 
 
 //GET /quizes/:id
 exports.show=function(req,res){
-  models.Quiz.find(req.params.quizId).then(function(quiz){
     res.render('quizes/show', {quiz: req.quiz});
-  })
 };
 
 //GET /quizes/:id/answer
 exports.answer=function(req,res){
-  models.Quiz.find(req.params.quizId).then(function(quiz){
-      if (req.query.respuesta === req.quiz.respuesta){
-        res.render('quizes/answer', {quiz:req.quiz, respuesta: 'Correcto'});
-      } else{
-        res.render('quizes/answer', {quiz:req.quiz, respuesta: 'Incorrecto'});
-      }
-  })
+    if (req.query.respuesta === req.quiz.respuesta){
+      res.render('quizes/answer', {quiz:req.quiz, respuesta: 'Correcto'});
+    } else{
+      res.render('quizes/answer', {quiz:req.quiz, respuesta: 'Incorrecto'});
+    }
 };
 /*
 //GET /quizes/question
