e7cd51f0b8c73715fec881f58d06cc7078935590
Pedro Manuel <pedrom.perezm@gmail.com>
2015-08-02 12:08:47 +0100

    Preguntas dinámicas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,3 +1,30 @@
+
+//GET /quizes
+exports.index=function(req,res){
+  models.Quiz.findAll().then(function(quizes){
+    res.render('quizes/index.ejs', {quizes: quizes});
+  })
+};
+
+
+//GET /quizes/:id
+exports.show=function(req,res){
+  models.Quiz.find(req.params.quizID).then(function(quiz){
+    res.render('quizes/show', {quiz: quiz});
+  })
+};
+
+//GET /quizes/:id/answer
+exports.answer=function(req,res){
+  models.Quiz.find(req.parms.quizId).then(function(quiz){
+      if (req.query.respuesta === quiz.respuesta){
+        res.render('quizes/answer', {quiz:quiz, respuesta: 'Correcto'});
+      } else{
+        res.render('quizes/answer', {quiz:quiz, respuesta: 'Incorrecto'});
+      }
+  })
+};
+/*
 //GET /quizes/question
 
 exports.question = function(req, res){
@@ -12,7 +39,7 @@ exports.answer = function(req, res) {
       res.render('quizes/answer', {respuesta: 'Incorrecto'});
    }
 };
-
+*/
 //GET /quizes/author
 exports.author = function(req, res){
   res.render('quizes/author');
